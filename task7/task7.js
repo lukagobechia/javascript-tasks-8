bf6a021fe17f4a4d3b8493c1c37d8810695ed591
Luka Gobechia <lukagobechiaa@gmail.com>
2024-10-20 19:03:17 +0400

    updated task 7

--- task7/task7.js
+++ task7/task7.js
@@ -2,18 +2,22 @@ const en = document.querySelector("#en");
 const ka = document.querySelector("#ka");
 const text = document.querySelector("#text");
 
-const init = () => {
-  if (localStorage.getItem("language") === "en") enClicked();
-  else if (localStorage.getItem("language") === "ka") kaClicked();
-  else enClicked();
+const enClicked = () => {
+    localStorage.setItem("language", "en");
+    text.innerHTML = "Hello";
 };
-en.addEventListener("click", () => {
-  localStorage.setItem("language", "en");
-  text.innerHTML = "Hello";
-});
-ka.addEventListener("click", () => {
-  localStorage.setItem("language", "ka");
-  text.innerHTML = "გამარჯობა";
-});
 
-init();
+const kaClicked = () => {
+    localStorage.setItem("language", "ka");
+    text.innerHTML = "გამარჯობა";
+};
+
+const init = () => {
+    if (localStorage.getItem("language") === "en") enClicked();
+    else if (localStorage.getItem("language") === "ka") kaClicked();
+    else enClicked();
+};
+
+en.addEventListener("click", enClicked);
+ka.addEventListener("click", kaClicked);
+document.addEventListener("DOMContentLoaded", init)
