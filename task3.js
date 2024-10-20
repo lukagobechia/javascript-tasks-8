adcb08e9aa12c8a46ea238a7d008ced661e9827d
Luka Gobechia <lukagobechiaa@gmail.com>
2024-10-20 19:05:56 +0400

    updated task 3

--- task3.js
+++ task3.js
@@ -1,5 +1,6 @@
 function GeNumber(phone) {
-    const regex = /^598-\d{2}-\d{2}-\d{2}$/;
-    console.log(regex.test(phone))
+    const regex = /^5\d{2}-\d{2}-\d{2}-\d{2}$/;
+    console.log(regex.test(phone));
 }
-GeNumber('598-45-45-45')
+
+GeNumber('698-45-45-45');
